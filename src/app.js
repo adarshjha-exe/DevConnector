62e31979f29f35006bb843d68b75a34aba7b932d
Adarsh Jha <jha01adarsh@gmail.com>
2026-05-11 13:27:45 +0530

    Middleware chaining

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const { adminAuth, userAuth } = require('./middlewares/auth.middleware');
+app.use('/admin', adminAuth);
 
-app.listen(3000, (err) => {
-  console.log('App is running');
+app.get('/user/profile', userAuth, (req, res) => {
+  res.send('All user fetched');
+});
+
+app.get('/admin/getAllUsers', (req, res) => {
+  res.send('All user fetched');
 });
 
 app.listen(3000, (err) => {
