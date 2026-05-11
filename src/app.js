53f93db2744d7a872c98b61e655086de4504103b
Adarsh Jha <adarshj@Adarshs-Mac-mini.local>
2026-05-11 13:57:02 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -13,6 +13,24 @@ app.get('/admin/getAllUsers', (req, res) => {
   res.send('All user fetched');
 });
 
+//error handling
+app.get('/user', (req, res) => {
+  // try {
+  //logic for finding the user
+  throw new Error('Random error');
+  // } catch (error) {
+  //   console.log(error.message);
+  //   res.status(401).send('Error occurred');
+  // }
+});
+
+//error handler : Always in the last for uncaught err
+app.use('/', (err, req, res, next) => {
+  if (err) {
+    res.send('Something Broke');
+  }
+});
+
 app.listen(3000, (err) => {
   console.log('App is running');
 });
