dc9367df3566641b90e92760e967479278d2c9f2
Adarsh Jha <adarshj@Adarshs-Mac-mini.local>
2026-05-28 21:45:07 +0530

    Add schema validation

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,25 +1,56 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 50,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!['Male', 'Female', 'Others'].includes(value)) {
+          throw new Error('Invalid Gender type');
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: 'https://www.mjunction.in/wp-content/uploads/2020/09/Dummy.jpg',
+    },
+    about: {
+      type: String,
+      default: 'This is default about',
+    },
+    skills: [String],
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 const User = mongoose.model('User', userSchema);
 
